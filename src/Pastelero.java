5d0e6835fa539b1dbda5d46b03792cad424a8ce5
Salvador Machta <smachta1@hotmail.com>
2015-10-17 16:50:55 -0430

    Hola
    f

---
+++ src/Pastelero.java
@@ -0,0 +1,25 @@
+
+
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+public class Pastelero extends Thread {
+
+    Almacen al; 
+    Semaphore 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
