4d4234d0909442f3f5626ea5af203d1ddf73a216
ariosgarriga <ariosgarriga@gmail.com>
2015-10-17 18:01:50 -0430

    10

--- src/Empleado.java
+++ src/Empleado.java
@@ -13,22 +13,47 @@
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class Empleado extends Thread{
     
     
-    private Almacen caja;
+    private Almacen Caja;
     private JTextArea ventana;
-    private Semaphore Vacio;
-    private Semaphore Comprado;
+    private Semaphore CapDisponible;
+    private Semaphore Ingrediente;
+    private int tiempo;
+    private String Alimento;
     
-    public Empleado(String nombre, Semaphore Vacio, Semaphore Comprado, Almacen Al){
-        super (nombre);
-        this.Vacio=Vacio;
-        this.Comprado=Comprado;
-        this.ven
+    
+    
+    
+    public Empleado(String nombreEmp, Semaphore Cap, Semaphore Ing, Almacen Caja, JTextArea Ventana, int tiempo, String Alimento){
+        super (nombreEmp);
+        this.CapDisponible=Cap;
+        this.Ingrediente=Ing;
+        this.ventana= Ventana;
+        this.Caja = Caja;
+        this.tiempo = tiempo*1000;
+        this.Alimento = Alimento;
+    }
+    
+    public void run(){
+            
+        try {
+            while(true){
+                
+                CapDisponible.acquire();
+                Thread.sleep(tiempo);
+                
+                Ingrediente.release();
+                ventana.append(super.getName()+" COMPRO "+Alimento+"/n");
+            }              
+        } catch (InterruptedException ex) {
+           Logger.getLogger(Empleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     
     }
 
